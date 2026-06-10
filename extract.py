65d68bdd6bd0ae42572387712f18055e65a3e1b4
Orhan Kürşad YAVUZLU <34066674+kursadyavuzlu@users.noreply.github.com>
2026-06-10 12:38:36 +0300

    refactor: add timeout and exception handling to extract script

--- extract.py
+++ extract.py
@@ -2,10 +2,18 @@ import requests
 
 url="https://api.frankfurter.dev/v2/rates"
 
-response = requests.get(url)
+#response = requests.get(url, timeout=10)
 
-if response.status_code == 200:
+#if response.status_code == 200:
+#    print(response.json())
+#else:
+#    print("FAIL REQUEST, Status Code: {response.status_code}")
+
+try:
+    response = requests.get(url, timeout=10)
     print(response.json())
-else:
-    print("FAIL REQUEST, Status Code: {response.status_code}")
+    print("\n Succesful.")
+except requests.exceptions.RequestException as e:
+    print("ERROR! : {e}")
+
 
